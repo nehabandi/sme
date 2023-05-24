afc228ac07bddbd64ed159a6b34fb934929a1305
nehabandi <nehabandi59@gmail.com>
2023-05-24 10:48:02 +0530

    hi

---
+++ reader.c
@@ -0,0 +1,31 @@
+#include "ab.h"
+main() 
+    { 
+       int fd;
+       char str[100];   
+       unlink("aPipe");  /* Remove named pipe if it already exists */ 
+       mknod("aPipe", S_IFIFO, 0);  /* Create name pipe */ 
+       chmod("aPipe", 0660);  /* Change its permissions */ 
+       fd = open("aPipe", O_RDONLY);  /* Open it for reading */ 
+        while(readLine(fd, str) );  /* Display received messages */ 
+          printf("%s\n", str); 
+       close(fd);   /* Close pipe */ 
+     }
+
+    int readLine( fd, str ) 
+     int fd; 
+     char* str; 
+     /* Read s single NULL-terminated line into str from fd */ 
+     /* Return 0 when the end of input is reached and 1 otherwise */
+     { 
+        int n; 
+          do /* Read characters until NULL or end of input */ 
+           { 
+              n = read( fd, str, 1);  /* Read one character */ 
+           } 
+          while ( n>0 && *str++ != NULL ); 
+
+        return ( n> 0 );  /* Return false if end of input */ 
+      } 
+
+
